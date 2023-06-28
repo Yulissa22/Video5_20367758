c3483f8a4ef2c9656466a623990a2648e56184a6
MINEDUCYT <MINEDUCYT@DESKTOP-OAO2BUA>
2023-06-27 21:39:49 -0600

    Agregar archivos de proyecto.

---
+++ Video5_20367758/Video5_20367758/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Video5_20367758
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Click Again";
+            (sender as Button).BackgroundColor = Color.Brown;
+        }
+    }
+}
